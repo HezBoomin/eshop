b25553cd9c1132d115762663f31eb20db1578ad6
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2024-02-06 12:05:11 +0700

    Adding ProductRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductRepositoryTest {
+
+    @InjectMocks
+    ProductRepository productRepository;
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+
+    }
+}
