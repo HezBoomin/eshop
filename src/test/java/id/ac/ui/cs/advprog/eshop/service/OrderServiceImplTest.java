64776566cd8d36c45fe37ae0b459366c40dd5fc4
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2024-03-05 21:27:01 +0700

    [RED] Add OrderServiceImpl skeletons.

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -56,7 +57,7 @@ class OrderServiceImplTest {
         doReturn(order).when(orderRepository).findById(order.getId());
 
         assertNull(orderService.createOrder(order));
-        verify(orderRepository, times(0)).save(order);
+        verify(orderRepository, times(1)).save(order);
     }
 
     @Test
@@ -107,7 +108,6 @@ class OrderServiceImplTest {
     @Test
     void testFindByIdNotFound() {
         doReturn(null).when(orderRepository).findById("zczc");
-
         assertNull(orderService.findById("zczc"));
     }
 
@@ -126,7 +126,7 @@ class OrderServiceImplTest {
     @Test
     void testFindAllByAuthorIfAllLowercase() {
         Order order = orders.get(1);
-        doReturn(new ArrayList<>()).when(orderRepository)
+        doReturn(new ArrayList<Order>()).when(orderRepository)
                 .findAllByAuthor(order.getAuthor().toLowerCase());
 
         List<Order> results = orderService.findAllByAuthor(
