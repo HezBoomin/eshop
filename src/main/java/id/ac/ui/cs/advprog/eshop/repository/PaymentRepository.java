d37f82ab3c5daacf6c2eb029e34666c74e66ddd5
Hezekial Octora Yudha Tampubolon <hezekial7778@gmail.com>
2024-03-07 14:30:52 +0700

    [GREEN] Implement PaymentRepositorty class, mistakes on lass commits as it should be PaymentRepository not OrderRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,7 +10,26 @@ import java.util.List;
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
-    public Payment save(Payment payment) {return null;}
+    public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i++;
+        }
+        paymentData.add(payment);
+        return payment;
+    }
 
-    public Payment findById(String id) {return null;}
+    public Payment findById(String id) {
+        for (Payment payment : paymentData) {
+            if (payment.getId().equals(id)) {
+                return payment;
+            }
+        }
+        return null;
+    }
 }
